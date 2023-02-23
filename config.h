67a4ed30224047be74d22f79f900546605c30745
wyzarch <wyz2016zxc@outlook.com>
2023-02-23 23:14:38 +0800

    change font etc

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 /* static const unsigned int systrayonleft = 0;    /1* 0: systray in the right corner, >0: systray on left of status text *1/ */
@@ -11,15 +11,15 @@ static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const double defaultopacity  = 0.9;
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "FiraCode Nerd Font Mono:size=14" };
+static const char dmenufont[]       = "monospace:size=14";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
 static const char col1[]            = "#ffffff";
-static const char col2[]            = "#ffffff";
+static const char col2[]            = "#005577";
 static const char col3[]            = "#ffffff";
 static const char col4[]            = "#ffffff";
 static const char col5[]            = "#ffffff";
@@ -41,7 +41,9 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+/*   󰈦 󰇩               ﬐    */
+static const char *tags[] = { "", "󰇩", "", "","","","﬏","" };
+/* static const char *tags[] = { "", "", "", "", "", "", "", "﬏", "", "" }; */
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -85,10 +87,14 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *rofirun[] = {"rofi","-show","run"};
+static const char *rofidrun[] = {"rofi","-show","drun"};
 
 #include "exitdwm.c"
 static Key keys[] = {
 	/* modifier                     key        function        argument */
+	{ MODKEY,                       XK_r,      spawn,          {.v = rofirun } },
+	{ MODKEY|ShiftMask,             XK_r,      spawn,          {.v = rofidrun } },
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
