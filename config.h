ada1f1819f19d20fdb380141684a140d873a9bdb
wyzarch <wyz2016zxc@outlook.com>
2023-02-23 22:26:53 +0800

    apply opacity

--- config.h
+++ config.h
@@ -10,6 +10,7 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const double defaultopacity  = 0.9;
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
@@ -47,9 +48,10 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	/* class      instance    title       tags mask     isfloating	 opacity	monitor */
+	{ "Gimp",     NULL,       NULL,       0,            1,           1.0,		-1 },
+	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           1.0,		-1 },
+	{ "St",	      NULL,       NULL,       0,            0,           defaultopacity, -1},
 };
 
 /* layout(s) */
@@ -110,6 +112,8 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	{ MODKEY|ShiftMask,		XK_o, changeopacity,	{.f = +0.1}},
+	{ MODKEY|ShiftMask,		XK_p, changeopacity,  {.f = -0.1}},
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
