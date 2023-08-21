c0d68a52f55d6f4fdc978aef873a4a089adc767d
arch23 <wyz2016zxc@outlook.com>
2023-08-21 17:37:10 +0800

    change keys

--- config.h
+++ config.h
@@ -103,8 +103,8 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
-	// { MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
-	{ MODKEY,             XK_q,      killclient,     {0} },
+	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
+	// { MODKEY,             XK_q,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_g,      setlayout,      {.v = &layouts[3]} },
 	{ MODKEY|ShiftMask,                       XK_f,      setlayout,      {.v = &layouts[1]} },
@@ -145,7 +145,7 @@ static const Key keys[] = {
 
 	{ MODKEY|ShiftMask,             XK_e,      exitdwm,       {0} },
   { MODKEY|ControlMask|ShiftMask, XK_q,      quit,           {1} }, 
-	// { MODKEY|ControlMask,           XK_q,      quit,           {0} },
+	{ MODKEY|ControlMask,           XK_q,      quit,           {0} },
 };
 
 /* button definitions */
