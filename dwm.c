9374e924d1aa26fc5fd9683a24e009c24ac4b4ef
arch23 <wyz2016zxc@outlook.com>
2023-09-07 20:28:46 +0800

    change autostart path

--- dwm.c
+++ dwm.c
@@ -1845,8 +1845,7 @@ run(void)
 
 void
 runAutostart(void) {
-	system("cd ~/.dwm; ./autostart_blocking.sh");
-	system("cd ~/.dwm; ./autostart.sh &");
+	system("cd ~/scripts; ./autostart.sh &");
 }
 
 void
