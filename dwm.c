670794ebfd10bffb500d596b8514facb472bcf04
arch24 <wyz2016zxc@outlook.com>
2024-07-17 15:40:08 +0800

    modify float sign

--- dwm.c
+++ dwm.c
@@ -1312,8 +1312,8 @@ void drawbar(Monitor *m) {
           // drw_clr_create(drw, &drw->scheme[ColFg], tagscheme[i][ColFg]);
 
     drw_text(drw, x, 0, w, bh, lrpad / 2, tags[i], urg & 1 << i);
-    // if ( m->tagset[m->seltags] & 1 << i) 
-    //   drw_rect(drw, x + ulinepad, bh - ulinestroke - ulinevoffset, w - (ulinepad * 2), ulinestroke, occ & 1 << i , urg & 1 << i);
+    if ( m->tagset[m->seltags] & 1 << i) 
+      drw_rect(drw, x + ulinepad, bh - ulinestroke - ulinevoffset, w - (ulinepad * 2), ulinestroke, occ & 1 << i , urg & 1 << i);
     x += w;
   }
 
@@ -1344,7 +1344,8 @@ void drawbar(Monitor *m) {
         }
         drw_text(drw, x, 0, tabw, bh, lrpad / 2, c->name, 0);
       if (c->isfloating)
-        drw_rect(drw, x + boxs, boxs, boxw, boxw, m->sel ==c, 0);
+        // drw_rect(drw, x + boxs, boxs, boxw, boxw, m->sel ==c, 0);
+        drw_rect(drw, x + 2 , 4 , 2 , bh -8 , 1, 0);
         x += tabw;
       }
     }
